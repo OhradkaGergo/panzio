a286d3bb6681e04b89aac094b79d3c68c9a92e68
OhradkaGergo <ohradkagergo@turr.hu>
2024-05-23 18:02:07 +0200

    scriptum renovatio

---
+++ app.js
@@ -0,0 +1,65 @@
+function Szamitas() {
+    let erkezes = new Date(document.getElementById("erkD").value);
+    let erkezesNap = erkezes.getDate().toString();
+    let erkezesHonap = erkezes.getMonth() + 1;
+    let erkezesEv = erkezes.getFullYear().toString();
+    let tavozas = new Date(document.getElementById("tavD").value);
+    let tavozasNap = tavozas.getDate().toString();
+    let tavozasHonap = tavozas.getMonth() + 1;
+    let tavozasEv = tavozas.getFullYear().toString();
+    let toltottIdo = ((tavozas-erkezes) / 3600000 ) /24;
+    let szobaT;
+    let vendegekSz = document.getElementById("fo").value;
+    let ellatas;
+    let furdo;
+    let penz;
+    
+    if(document.getElementById("egyagy").checked == true){
+        szobaT = "Egyágyas";
+        penz += 9000 * toltottIdo;
+    }
+    else if(document.getElementById("ketagy").checked == true){
+        szobaT = "Kétágyas";
+        penz += 15000 * toltottIdo;
+    }
+    else if(document.getElementById("ketagy+1").checked == true){
+        szobaT = "Kétágyas 1 pótággyal";
+        penz += 18000 * toltottIdo;
+    }
+    else if(document.getElementById("ketagy+2").checked == true){
+        szobaT = "Kétágyas 2 pótággyal";
+        penz += 18000 * toltottIdo;
+    }
+
+    if(document.getElementById("reggeli").checked == true){
+        ellatas = "Reggeli";
+        penz += 800 * toltottIdo;
+    }
+    else if(document.getElementById("felpanzio").checked == true){
+        ellatas = "Félpanzió";
+        penz += 800 * toltottIdo;
+    }
+    else if(document.getElementById("teljespanzio").checked == true){
+        ellatas += "Teljes panzió";
+        penz += 800 * toltottIdo;
+    }
+    
+    if(document.getElementById("furdo1").checked == true){
+        furdo += " Beltéri medencék";
+        penz += 800 * toltottIdo;
+    }
+    if(document.getElementById("furdo2").checked == true){
+        furdo += " Kültéri medencék";
+        penz += 800 * toltottIdo;
+    }
+    if(document.getElementById("furdo3").checked == true){
+        furdo += " Szauna belépő";
+        penz += 800 * toltottIdo;
+    }
+    if(document.getElementById("furdo4").checked == true){
+        furdo += " Teljes belépő";
+        penz += 2000 * toltottIdo;
+    }
+
+    alert("Kedves vendég. \n\nTájékoztatjuk a sikeres folgalásáról; \n\n Érkezés: " + erkezesEv + " - " + erkezesHonap + " - " + erkezesNap + " \n\n Távozás: " + tavozasEv + " - " + tavozasHonap + " - " + tavozasNap + " \n\n Szoba típusa: " + szobaT + " \n\n Vendégek száma: " + vendegekSz + " \n\n Ellátás: " + ellatas + " \n\n Igényelt Szolgáltatások: " +  furdo + " \n\n Teljes összeg: " + penz + "\n\n\nKöszönjük a megrendelését.")
+}
